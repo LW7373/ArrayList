577305f54444574d2ff07c98d52df04957d50029
MD3729 <111062288+MD3729@users.noreply.github.com>
2023-10-03 18:12:42 -0400

    Mistakes were made

--- MeatTypeEnum.java
+++ MeatTypeEnum.java
@@ -12,6 +12,6 @@ public class MeatTypeEnum {
     
     // Create enum for meat type
     public enum MeatType{
-        beef, pork, chicken, fish, turkey, vegan, lamb, mystery, 
+        beef, pork, chicken, fish, turkey, vegan, lamb, mystery 
     }
 }
